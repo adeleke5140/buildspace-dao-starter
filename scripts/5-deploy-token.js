2aa9cfc6b5dcc00b67c618e2f130ad5ab60beadd
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-01-08 21:17:08 +0100

    has deploy token script file added

---
+++ scripts/5-deploy-token.js
@@ -0,0 +1,18 @@
+import sdk from "./1-initialize-sdk.js";
+
+const app = sdk.getAppModule("0xb9Ca1c900363bDeC84C85D115e840a09EE825Ef4");
+
+(async () => {
+  try {
+    const tokenModule = await app.deployTokenModule({
+      name: "JDOA Governance Token",
+      symbol: "DAVE",
+    });
+    console.log(
+      "✅ successfully deployed token module, address:",
+      tokenModule.address
+    );
+  } catch (error) {
+    console.error("failed to deploy token module", error);
+  }
+})();
